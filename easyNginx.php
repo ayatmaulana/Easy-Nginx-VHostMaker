ef52e0f24a520c46019a6b0ab22e2693ba685d51
AyatMaulana <Ayatmaulana.indosystem@gmail.com>
2016-09-11 20:07:14 +0700

    menambah nginx configuration dan unix color

--- easyNginx.php
+++ easyNginx.php
@@ -4,7 +4,7 @@
 * Simple Nginx VHost Creator
 */
 
-if (isset($_SERVER['REMOTE_ADDR'])) die('Permission Denied');
+if (isset($_SERVER['REMOTE_ADDR'])) die('Permission Denied CLI Only :)');
 
 class NginxVhostCreator
 {
@@ -17,7 +17,7 @@ class NginxVhostCreator
 	}
 
 	public function getConfigData(){
-		$word = ['server','port','path','htaccess'];
+		$word = ['server','port','path'];
 		$data = [];
 		for ($i=0; $i < count($word); $i++) { 
 
@@ -67,14 +67,86 @@ class NginxVhostCreator
 
 		return $data;
 	}
+
+	public function makeFileConfig(){
+
+		$config = "server {
+			listen ".trim($this->datanya['port']).";
+
+			root ".trim($this->datanya['path']).";
+			index index.php index.html index.htm ;
+
+			# Make site accessible from http://localhost/
+			server_name ".trim($this->datanya['server']).";
+
+
+		    location ~* \.(ico|css|js|gif|jpe?g|png)(\?[0-9]+)?$ {
+	            expires max;
+	            log_not_found off;
+		    }
+		    
+		    location / {
+		            # Check if a file or directory index file exists, else route it to index.php.
+		            try_files ".'\$'."uri ".'\$'."uri/ /index.php;
+		    }";
+
+		$config .= "
+			# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+			#
+			location ~ \.php$ {
+				fastcgi_split_path_info ^(.+\.php)(/.+)$;
+			#
+			#	# With php5-cgi alone:
+			#	fastcgi_pass 127.0.0.1:9000;
+			#	# With php5-fpm:
+				fastcgi_pass unix:/var/run/php5-fpm.sock;
+				fastcgi_index index.php;
+				include fastcgi_params;
+			}
+		}";
+
+		exec("echo \"".$config."\" > ~/".trim($this->datanya['server']));
+		exec("sudo mv ~/". trim($this->datanya['server'])."  /etc/nginx/sites-enabled/".$this->datanya['server']);
+		exec("sudo ln -s /etc/nginx/sites-enabled/".trim($this->datanya['server'])." /etc/nginx/sites-avaible"); 
+		exec("sudo nginx -t");
+		exec("sudo service nginx restart");
+
+		return;
+	}
+
+
+	public static function color($string, $fontColor=NULL) {
+		switch ($fontColor) {
+			case 'black' : $color = '0;30'; break;
+			case 'dark_gray' : $color = '1;30'; break;
+			case 'blue' : $color = '0;34'; break;
+			case 'l_blue' : $color = '1;34'; break;
+			case 'green' : $color = '0;32'; break;
+			case 'l_green' : $color = '1;32'; break;
+			case 'cyan' : $color = '0;36'; break;
+			case 'l_cyan' : $color = '0;36'; break;
+			case 'red' : $color = '0;31'; break;
+			case 'l_red' : $color = '1;31'; break;
+			case 'purple' : $color = '0;35'; break;
+			case 'l_purple' : $color = '1;35'; break;
+			case 'brown' : $color = '0;33'; break;
+			case 'yellow' : $color = '1;33'; break;
+			case 'l_gray' : $color = '0;37'; break;
+			case 'white' : $color = '1;37'; break;
+		}
+		$colored_string = "";
+		$colored_string .= "\033[" . $color . "m";
+		$colored_string .=  $string . "\033[0m";
+		return $colored_string;
+	}
 }
 
 
 
-$show = "\tSimplify Your Life :) \n";
-$show .= "\t----------------------\n";
-$show .= "\tEasy Nginx Vhost Config Maker\n";
-$show .= "\t(c) Ayat Maulana 2016\n\n";
+$show = "\t".NginxVhostCreator::color("Simplify Your Life :)","yellow")."\n";
+$show .= "\t".NginxVhostCreator::color("--------------------","red")."\n";
+$show .= "\t".NginxVhostCreator::color("Easy Nginx Vhost Config Maker","l_blue")."\n";
+$show .= "\t".NginxVhostCreator::color("(c) Ayat Maulana 2016","l_blue")."\n\n";
 
 echo $show;
 (new NginxVhostCreator());
---
+++ kakangprabu.com
@@ -0,0 +1,1 @@
+/etc/nginx/sites-enable/kakangprabu.com
